f1ce2d3f284619289725e52474e761126a85341c
refren86 <denis1975shvaiber@gmail.com>
2022-03-30 23:19:19 +0300

    Finished first task

--- task1/index.js
+++ task1/index.js
@@ -1,6 +1,23 @@
-function calculate() {
-  console.log("commit");
-  console.log("commit2");
-  console.log("commit3");
-  return;
-}
+const { log } = console;
+
+const calculate = (dotsToCalculate) => {
+  const splitter = dotsToCalculate.split(" ");
+  const leftDots = splitter[0];
+  const operator = splitter[1];
+  const rightDots = splitter[2];
+
+  let dots = "";
+  const dotsCount = eval(`${leftDots.length} ${operator} ${rightDots.length}`);
+
+  if (dotsCount < 0) {
+    return "The result of calculation is less than 0";
+  }
+
+  for (let i = 0; i < dotsCount; i++) {
+    dots += ".";
+  }
+
+  return dots;
+};
+
+// log(calculate("..... // ."));
