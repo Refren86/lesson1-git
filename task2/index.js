e31e1a5916e32b95a1fe233f5f2ff2b287b5d468
refren86 <denis1975shvaiber@gmail.com>
2022-03-30 23:26:21 +0300

    Finished second task

--- task2/index.js
+++ task2/index.js
@@ -1,4 +1,45 @@
-function calculate() {
-  //Here is your solution
-  return
-}
+const { log } = console;
+// FIRST SOLUTION TO TASK 2:
+
+// const grid = (num) => {
+//   if (num < 0) {
+//     return null;
+//   }
+
+//   let result = "";
+//   const alphabet = "abcdefghijklmnopqrstuvwxyz".repeat(num);
+
+//   for (let i = 0; i < num; i++) {
+//     for (let j = 0; j < num; j++) {
+//       result += alphabet[j + i];
+//     }
+//     result += "\n";
+//   }
+
+//   return result;
+// };
+
+// log(grid(20));
+
+// --------------------------------------------------------------------------
+// SECOND SOLUTION TO TASK 2:
+
+const grid = (num) => {
+  if (num < 0) {
+    return null;
+  }
+
+  let result = "";
+  const firstLetterCharcoded = "a".charCodeAt();
+
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num; j++) {
+      result += String.fromCharCode(firstLetterCharcoded + ((i + j) % 26));
+    }
+    result += "\n";
+  }
+
+  return result;
+};
+
+log(grid(25));
